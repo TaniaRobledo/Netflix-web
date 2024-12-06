d9e44c78fd1995eb83efee37683f75fd5a7f4f95
TaniaRobledo <tania.robledoabenojar@riberadeltajo.es>
2024-12-06 21:04:46 +0100

    Página información acabada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-// Selecciona los elementos
+
 const alertBox = document.getElementById("alert");
 const closeBtn = document.getElementById("close-alert");
 
-// Añade el evento de clic al botón de cierre
 closeBtn.addEventListener("click", () => {
-  alertBox.style.display = "none"; // Oculta el mensaje sin afectar al contenedor
+  alertBox.style.display = "none"; 
 });
