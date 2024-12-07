ade4024ff66660ed0e8a2cdd7993a59308939cf1
TaniaRobledo <tania.robledoabenojar@riberadeltajo.es>
2024-12-07 10:42:51 +0100

    Sección peliculas y series acabado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-
+// Alert para la página de información
 const alertBox = document.getElementById("alert");
 const closeBtn = document.getElementById("close-alert");
 
 closeBtn.addEventListener("click", () => {
   alertBox.style.display = "none"; 
 });
+
+
